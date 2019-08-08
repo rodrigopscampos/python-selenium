03f8bec2779c9e2f2152c53be9d82dcc9ae4dfc2
Rodrigo Campos <rodigopscampos@gmail.com>
2019-08-08 19:53:14 -0300

    adiciona exercicios

--- exercicios/ex8.py
+++ exercicios/ex8.py
@@ -12,6 +12,9 @@ inputs = navegador.find_elements_by_tag_name('input')
 txt_numero = inputs[0]
 btn_processar = inputs[1]
 
+#txt_numero = [el for el in inputs if el.get_attribute('type') == "text"][0]
+#btn_processar = [el for el in inputs if el.get_attribute('type' == "button")][0]
+
 txt_numero.send_keys(n)
 btn_processar.click()
 
